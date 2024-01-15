b25b0735aeddba6b54813ff95249c3f654feae9b
kukutulu <nguoi0123@gmail.com>
2024-01-16 01:18:53 +0700

    settings modal

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -26,9 +26,11 @@ import { toast } from "sonner";
 import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
+import { useSettings } from "@/hooks/use-settings";
 
 export const Navigation = () => {
   const search = useSearch();
+  const settings = useSettings();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -147,7 +149,7 @@ export const Navigation = () => {
         <div>
           <UserItem />
           <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
-          <Item label="Settings" icon={Settings} onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
           <Item onClick={handlerCreate} label="New Page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
