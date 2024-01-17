4beedde785e571011f34a80c716325237b3f5280
kukutulu <nguoi0123@gmail.com>
2024-01-18 03:50:47 +0700

    add toolbar component (Icon, Title)

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,40 @@
-const DocumentIdPage = () => {
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
   return (
-    <div>
-      <p>Document Id Page</p>
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
     </div>
   );
 };
